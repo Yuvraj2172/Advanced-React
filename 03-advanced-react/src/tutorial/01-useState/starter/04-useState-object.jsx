d9085a6d02c77ab7d478a7c155ffeb9b66b6b139
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-28 00:58:38 +0530

    UseState Object

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
 
 const UseStateObject = () => {
+  const [person , setPerson] = useState({
+    name : 'Peter',
+    age : 24,
+    hobby : 'Read Books'
+  })
   const [name, setName] = useState("peter");
   const [age, setage] = useState(24);
   const [hobby, setHobby] = useState("read books");
   
   const displayPerson =()=>{
-    setName('John');
-    setage(28);
-    setHobby('Scream at the computer');
-  }
+    // setName('John');
+    // setage(28);
+    // setHobby('Scream at the computer');
+    // setPerson({name : 'Yuvraj', age : 20, hobby: 'Studying'});
+
+    // setPerson("shakeandbake");  //application will break
+
+    // setPerson({name : 'Yuvraj'});
+
+    setPerson({...person , name : 'Yuvraj'});
+  };
 
   return <>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h3>Enjoys : {hobby}</h3>
-      <button className="btn" onClick={displayPerson} >Show John</button>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>Enjoys : {person.hobby}</h3>
+      <button className="btn" onClick={displayPerson} >Show Yuvraj</button>
       </>;
 };
 
