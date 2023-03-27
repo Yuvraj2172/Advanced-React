2fc8f8bb4a6fdb4ec9d694ed09a954e941ac9b33
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-28 01:40:10 +0530

    UseState Functional Update

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value , setValue] = useState(0);
   const handleClick =()=>{
-    setValue(value +1);
-    console.log(value);
+    // setValue(value +1);
+    setValue((currentState)=>{
+      const newState = currentState +1;
+      return newState;
+    });
+    // console.log(value);
   }
   return <>
     <div>
