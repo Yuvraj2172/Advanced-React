40ef890120df22b00b773c3f8673d8fc957bb103
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-27 22:57:36 +0530

    useState Array Example

--- 03-advanced-react/src/tutorial/01-useState/final/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/final/03-useState-array.jsx
@@ -1,32 +1,11 @@
-import React from 'react';
-import { data } from '../../../data';
-const UseStateArray = () => {
-  const [people, setPeople] = React.useState(data);
+import {data} from '../../../data';
+import React , {useState} from 'react';
 
-  const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
+const UseStateArray=()=>{
+  const [people, setPeople] = useState(data);
   return (
-    <div>
-      {people.map((person) => {
-        const { id, name } = person;
-        return (
-          <div key={id} className='item'>
-            <h4>{name}</h4>
-            <button onClick={() => removeItem(id)}>remove</button>
-          </div>
-        );
-      })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
-    </div>
-  );
-};
+    <h2>useState Array Exapmle</h2>
+  )
+}
 
 export default UseStateArray;
--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,36 @@
+import { data } from "../../../data";
+import React, { useState } from "react";
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people , setPeople] = useState(data);
+  const removeItem=(id)=>{
+   let newPeople = people.filter((person)=>person.id!==id);
+   setPeople(newPeople);
+    // console.log(id)
+  }
+  const clearAllItems=()=>{
+    setPeople([]);
+  }
+
+  const repopulate=()=>{
+    setPeople(data);
+  }
+
+  return (
+    <div>
+      {people.map((person)=>{
+        const {id, name } = person;
+        // console.log(person);
+        return <div key={id}>
+          <h4>{name}</h4>
+          <button type='button' className="btn" onClick={()=>{
+            removeItem(id);
+          }}  > remove</button>
+        </div>
+      })}
+      <button type ='button' style={{marginTop:'2rem'}} className="btn" onClick={clearAllItems} >Clear Items</button>
+      <button type ='button' style={{margin:'2rem'}} className="btn" onClick={repopulate} >Repopulate</button>
+    </div>
+  )
 };
 
 export default UseStateArray;
