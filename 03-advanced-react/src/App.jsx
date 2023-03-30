48640d4d48b164dbf9e57713af9d855e7cedb6b8
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-30 18:41:38 +0530

    UseEffect Fetch Challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,11 @@
-import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
+import Starter from './tutorial/02-useEffect/starter/04-fetch-data'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
       <Starter/>
-      <Final/>
+      {/* <Final/> */}
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,56 @@
-const url = 'https://api.github.com/users';
+import { useState, useEffect } from "react";
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  // const fetchUserData = () => {
+  //   fetch("https://api.github.com/users")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       setUsers(data);
+  //     });
+  // };
+  // useEffect(() => {
+  //   fetchUserData();
+  // }, []);
+
+  useEffect(()=>{
+    const fetchData=async ()=>{
+      try{
+        const response = await fetch(url);
+        const users = await response.json();
+        setUsers(users);
+        // console.log(users);
+      }
+      catch(error){
+        console.log(error)
+      }
+    };
+    fetchData();
+  },[]);
+
+  return (
+    <section>
+      <h2>Github Users</h2>
+      <ul className="users">
+      {users.map((user)=>{
+        const {id,login , avatar_url, html_url} = user;
+        // console.log(user)
+        return(
+          <li key={user.id}>
+            <img src={avatar_url} alt={login} style={{width:'50px'}} />
+            <div>
+              <h5>{login}</h5>
+              <a href={html_url}>profile</a>
+            </div>
+          </li>
+        )
+      })}
+      </ul>
+    </section>
+  );
 };
 export default FetchData;
