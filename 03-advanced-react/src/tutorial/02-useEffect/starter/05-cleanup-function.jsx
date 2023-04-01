539cffda29050d803d9d734e39b43c287420e916
Yuvraj Soni <demonsroar2172@gmail.com>
2023-04-01 18:17:54 +0530

    UseEffect Timer Example

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,20 +1,34 @@
 import { useEffect, useState } from "react";
 
 const CleanupFunction = () => {
-  const [value , setValue] = useState(false);
-  return (<div>
-    <button className="btn" onClick={()=>{setValue(!value)}}>Toggle</button>
-    {value && <SecondComponent />}
-  </div>);
+  const [value, setValue] = useState(false);
+  console.log('render');
+  return (
+    <div>
+      <button
+        className="btn"
+        onClick={() => {
+          setValue(!value);
+        }}
+      >
+        Toggle
+      </button>
+      {value && <SecondComponent />}
+    </div>
+  );
 };
 
-const SecondComponent=()=>{
-  useEffect(()=>{
-    console.log("Use Effect");
-    
-  },[])
-  return (
-    <h2>this is second Component</h2>
-  )
-}
+const SecondComponent = () => {
+  useEffect( () => {
+    // console.log("Use Effect");
+    const intId = setInterval(() => {
+      // console.log('hello from interval');
+    }, 1000);
+    return ()=>{
+      clearInterval(intId);
+      console.log('clean up')
+    }
+  }, []);
+  return <h2>this is second Component</h2>;
+};
 export default CleanupFunction;
