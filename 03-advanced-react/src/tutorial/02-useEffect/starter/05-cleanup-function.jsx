55f0caa4788da48362d9a8c054190e60c7b2f9ef
Yuvraj Soni <demonsroar2172@gmail.com>
2023-04-01 17:15:28 +0530

    UseEffect Cleanup Function

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [value , setValue] = useState(false);
+  return (<div>
+    <button className="btn" onClick={()=>{setValue(!value)}}>Toggle</button>
+    {value && <SecondComponent />}
+  </div>);
 };
 
+const SecondComponent=()=>{
+  useEffect(()=>{
+    console.log("Use Effect");
+    
+  },[])
+  return (
+    <h2>this is second Component</h2>
+  )
+}
 export default CleanupFunction;
