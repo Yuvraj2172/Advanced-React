308d3d27f7645fb124fac3ec29c7923aa3c113f7
Yuvraj Soni <demonsroar2172@gmail.com>
2023-04-01 13:08:25 +0530

    Short Circuit Exampole

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  //falsy
+  const [text, setText] = useState("");
+
+  //truthy
+  const [name, setName] = useState("Yuvraj");
+
+  const codeExample = text || 'hello world';
+
+  return (
+    <div>
+      {/* {if(someCondition){"Wont work"}} */}
+      <h4>Falsy OR : {text || "Hello World"}</h4>
+      <h4>Falsy AND : {text && "Hello World"}</h4>
+      <h4>Truthy OR : {name || "Hello World"}</h4>
+      <h4>Truthy AND : {name && "Hello World"}</h4>
+      {codeExample}
+    </div>
+  );
 };
 export default ShortCircuitOverview;
