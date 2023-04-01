b994c37feabb52c2c07e0b6e21ca6b62c9d0e419
Yuvraj Soni <demonsroar2172@gmail.com>
2023-04-01 16:42:30 +0530

    Toggle Challenge

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [value, setValue] = useState(false);
+  const changeState=()=>{
+    setValue(!value);
+    console.log(value)
+  }
+  return (
+    <div>
+    <button className="btn" onClick={changeState}>Click me </button>
+    {value && <ShowAlert name='Yuvraj' />}
+    </div>
+  );
 };
 
+  const ShowAlert=({name})=>{
+    console.log(name)
+    return (
+      <>
+      <h2>Conditionally Rendered Component</h2>
+      <h6>Built by {name} </h6>
+      </>
+    )
+  }
 export default ToggleChallenge;
