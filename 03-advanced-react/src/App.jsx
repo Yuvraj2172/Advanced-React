4632d4e48fb9014541cac1e4a7a024d05bf314e9
Yuvraj Soni <demonsroar2172@gmail.com>
2023-04-10 22:24:54 +0530

    Forms Setup

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -2,11 +2,14 @@
 // import Final from './tutorial/03-conditional-rendering/final/01-multiple-returns-basics'
 // import Navbar from './tutorial/04-project-structure/starter/Navbar.jsx'
 // import './tutorial/04-project-structure/starter/Navbar.css';
-import List from "./tutorial/05-leverage-javascript/starter/List";
+
+import ControlledInputs from "./tutorial/06-forms/starter/01-controlled-inputs";
+
+// import List from "./tutorial/05-leverage-javascript/starter/List";
 function App() {
   return (
     <div className='container'>
-      <List/>
+     <ControlledInputs/>
     </div>
   );
 }
