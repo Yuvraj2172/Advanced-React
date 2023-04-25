a77d560c520677a54ea865eaa9c9f77cca683f61
Yuvraj Soni <demonsroar2172@gmail.com>
2023-04-25 23:34:55 +0530

    Context API

--- 03-advanced-react/src/tutorial/09-context-api/starter/Navbar.jsx
+++ 03-advanced-react/src/tutorial/09-context-api/starter/Navbar.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react'
+import React, { createContext, useState } from 'react'
 import NavLinks from './NavLinks'
-
+export const NavbarContext = createContext();
+// console.log(NavbarContext.Provider);
 const Navbar = () => {
+    // React.createContext
     const [user, setUser] = useState({name : 'bob'});
     const logout=()=>{
         setUser(null);
     }
     console.log(user)
   return (
+    <NavbarContext.Provider value ={{user, logout}}>
    <nav className='navbar'>
     <h5>CONTEXT API</h5>
-    <NavLinks user={user} logout = {logout} />
+    <NavLinks  />
    </nav>
+   </NavbarContext.Provider>
   )
 }
 
