bba9bad3ef5766dc7efebcd2c6a1f27ee9f8d254
Yuvraj Soni <demonsroar2172@gmail.com>
2023-05-15 17:46:24 +0530

    UseTransition

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,8 @@
 import './index.css';
 // import Starter from './tutorial/10-useReducer/starter/01-useReducer';
 // import Starter from './tutorial/11-performance/starter/02-lower-state-challenge'
-import Starter from './tutorial/11-performance/starter/03-hooks/index';
+// import Starter from './tutorial/11-performance/starter/03-hooks/index';
+import Starter from './tutorial/11-performance/starter/04-react-18';
 import react from 'react';
 const  app = ()=>{
   return <div className='container'>
