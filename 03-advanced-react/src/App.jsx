0528b3d02ae83813625d71b2cb06dda551c2581e
Yuvraj Soni <demonsroar2172@gmail.com>
2023-05-15 16:01:02 +0530

    Lower State-Challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,6 @@
 import './index.css';
-import Starter from './tutorial/10-useReducer/starter/01-useReducer'
+// import Starter from './tutorial/10-useReducer/starter/01-useReducer';
+import Starter from './tutorial/11-performance/starter/02-lower-state-challenge'
 import react from 'react';
 const  app = ()=>{
   return <div className='container'>
