d0659c8ff3bb1a23694a89cb99b6f0817740d9f8
Yuvraj Soni <demonsroar2172@gmail.com>
2023-05-30 00:59:21 +0530

    global default

--- 05-axios-tutorial/src/App.js
+++ 05-axios-tutorial/src/App.js
@@ -1,10 +1,11 @@
-import Setup from "./examples/3-post-request"
-import Title from './components/Title';
+import Setup from "./examples/4-global-instance";
+import Title from "./components/Title";
+import "./axios/global";
 function App() {
   return (
     <main>
       <Title />
-      <Setup/>
+      <Setup />
     </main>
   );
 }
