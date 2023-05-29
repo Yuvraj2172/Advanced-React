6579e1f2a1b21553d2ec69745a0f751c6442a817
Yuvraj Soni <demonsroar2172@gmail.com>
2023-05-30 00:18:15 +0530

    axios init

---
+++ 05-axios-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import Setup from "./examples/1-first-request"
+import Title from './components/Title';
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup/>
+    </main>
+  );
+}
+
+export default App;
